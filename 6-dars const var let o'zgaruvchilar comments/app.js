fa77a855bcb7a3ada6dac60d84075adc7a19d5cb
Mirjalol Bakhtiyorov <123945148+mirjalolbakhtiyorov@users.noreply.github.com>
2024-10-31 08:21:01 +0300

    Add files via upload

---
+++ 6-dars const var let o'zgaruvchilar comments/app.js
@@ -0,0 +1,10 @@
+let ism = 'Mirjalol'
+// ism = 'Simon'
+let surname = 'Bakhtiyorov'
+let middle_name = 'Alishervich'
+console.log(ism)
+let full_name = ism +' '+surname+ ' '+  middle_name;
+console.log(full_name)
+
+// comments 
+/* ko'p qatorli commentlar */

a87c58f6c34d47378e6fd85c7d28faab1f609d19
Mirjalol Bakhtiyorov <123945148+mirjalolbakhtiyorov@users.noreply.github.com>
2024-10-31 08:22:57 +0300

    9-dars. string methods

---
+++ 9-dars. string methods/app.js
@@ -0,0 +1,35 @@
+// let givenName = 'Mirjalol';
+// let surname = 'Bakhtiyorov'
+// let patronymic = 'Alishervich'
+
+// string concatenation
+
+// let fullname = givenName + ' '+ surname+' '+patronymic;
+// console.log(fullname)
+
+// getting character
+
+// let email = 'mirjalolbakhtiyorov@gmail.com'
+// console.log(email[1]);
+// console.log(email[19])
+
+// string lenth
+
+// let email = 'mirjalolbakhtiyorov@gmail.com'
+// console.log(email.length)
+// let national = 'uzbek'
+// console.log(national.length)
+
+//string methods ===> function
+
+// let fullname = 'Mirjalol Bakhtiyorov Alishervich';
+// let result = fullname.toLocaleUpperCase()
+// console.log(result)
+// console.log(fullname.toLocaleLowerCase())
+
+
+// indexof 
+
+// let book = 'geography'
+// console.log(book.indexOf("e"))
+// console.log(book.indexOf('h'))

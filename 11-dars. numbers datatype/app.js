173e5a7f1ee13365a4fdc8fac78d511776fe6a1e
Mirjalol Bakhtiyorov <123945148+mirjalolbakhtiyorov@users.noreply.github.com>
2024-10-31 08:31:55 +0300

    11-dars. number data type
    11-dars. number data type

---
+++ 11-dars. numbers datatype/app.js
@@ -0,0 +1,33 @@
+// numbers 
+
+let number = [1, 2, 3, -5, 90, 98.3, 8374]
+console.log(number)
+
+// math operators
+// 
+// console.log(10/2)
+// console.log(10%2)
+// console.log(11%6)
+// console.log(8**7)
+// console.log(2**10)
+// 
+// let radius = 10;
+// const pi = 3.1457103
+// console.log(pi*radius**2)
+
+// like = 10;
+// like = like + 1;
+// like++
+// like+=10
+// like-=8
+// like*=5
+// console.log(like)
+
+// NAN xatoligi
+
+// let ism = 'Mirjalol'
+// let son = 2003
+// let result = ism+son
+// console.log(result)
+// console.log(56+"9")
+console.log(9/d)

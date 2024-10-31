e1a453ddb3f3d88c6d36645034c66e1587c2e469
Mirjalol Bakhtiyorov <123945148+mirjalolbakhtiyorov@users.noreply.github.com>
2024-10-31 08:28:12 +0300

    10-dars. string methods
    string metodlari

---
+++ 10-dars. string method continue/app.js
@@ -0,0 +1,37 @@
+let email = ' mirjalolbaxtiyorov03@gmail.com '
+
+// lastindexof()
+
+// let result = email.lastIndexOf('o')
+// console.log(result)
+
+// indexof()
+
+// let result = email.indexOf('a');
+// console.log(result)
+
+// slice()
+
+// let result =  email.slice(4,29);
+// console.log(result)
+
+//subst()
+
+// let result = email.substr(4,10)
+// console.log(result.length, email.length)
+
+// replace()
+
+// let result = email.replace('lol','khon')
+// console.log(result)
+
+// charAt()
+// 
+// console.log(email.charAt())
+
+// trim()
+// console.log(email.trim)
+
+// split()
+
+// console.log(email.split(''))

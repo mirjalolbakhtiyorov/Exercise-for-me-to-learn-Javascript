cecba0e98ef22a3df2cb0077a975737db29fd858
Mirjalol Bakhtiyorov <123945148+mirjalolbakhtiyorov@users.noreply.github.com>
2024-10-31 08:26:19 +0300

    10-dars. string methods
    string metodlari

---
+++ 10-dars full string methods/app.js
@@ -0,0 +1,74 @@
+// string 
+
+// let ism = 'Mirjalol'
+// let familiya = 'Bakhtiyorov'
+// let familyname = 'Alishervich'
+// string concatenation ==> stringlarni qo'shish
+
+// let result = ism+' '+familiya+' '+familyname;
+// console.log(result)
+
+// f{} fstring
+
+// console.log(`${ism},${familiya},${familyname}`)
+
+
+// stringlarni ichidan alohida harf yoki belgini ajratib olish
+let gmail = 'mirjalolbakhtiyorov@gmail.com'
+// let result  = gmail[8]
+// console.log(result)
+
+
+// string.length ===> stringlarni uzunligini hisoblash
+
+// let result = gmail.length
+// console.log(result)
+
+
+// string methods
+
+//touppercase
+// let result = gmail.toLocaleUpperCase()
+
+// tolowercase
+// console.log(gmail.toLowerCase())
+// console.log(result)
+
+// indexof()
+
+// let fullname = 'Mirjalol Bakhtiyorov Alishervich'
+
+// console.log(fullname.indexOf('ich'),gmail.length)
+
+//  console.log(fullname.lastIndexOf("v"))
+//  console.log(fullname.length)
+
+// slice
+
+// console.log(fullname.slice(0,8))
+
+//substr()
+
+// console.log(fullname.substr(8,fullname.length))
+
+// replace ==> ikta harfni o'rnini almashtiradi
+
+// console.log(fullname.replace('jalol','jaloljon '))
+
+// charAr()
+
+// console.log(fullname.charAt())
+ // trim()
+// let subject = '   Mirjalol   '
+// console.log(subject.trim().length,subject.length)
+
+
+// repeat ==> stringni takrorlash
+place = 'Kashkadaryo '
+console.log(place.repeat(9))
+
+
+
+// split =====> stringni arrivga almashtirish
+
+console.log(place.split(""))
